720478251e26ca91095292a43392ec864d0a12b6
keshavsavva <keshav.s.avva@gmail.com>
2020-04-08 14:00:34 -0400

    fixed linting errors should pass travis

--- models/favGameGenre.js
+++ models/favGameGenre.js
@@ -1,18 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-    const FavGameGenre = sequelize.define('FavGameGenre', {
-      gameGenres: {
-        type: DataTypes.STRING,
-        allowNull: false
-      } 
-    });
-  
-    FavGameGenre.associate = function(models) {
-        FavGameGenre.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
+  const FavGameGenre = sequelize.define("FavGameGenre", {
+    gameGenres: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
 
-    return FavGameGenre;
+  FavGameGenre.associate = function(models) {
+    FavGameGenre.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return FavGameGenre;
 };
--- public/js/questionaire.js
+++ public/js/questionaire.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(elems, options);
+});
 
-  // Or with jQuery
+// Or with jQuery
 
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+$(document).ready(function() {
+  $("select").formSelect();
+});
