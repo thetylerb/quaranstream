281f5da6c2afa4b42dd83edd137ab51b807712ae
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-04-08 17:02:06 -0400

    Added JS to questionaire page and changed id names

--- game_api.js
+++ game_api.js
@@ -1,19 +1,27 @@
-var axios = require("axios");
+const axios = require("axios");
 require("dotenv").config();
 //2-point and click,4-fighting,5-shooter,7-music,8-platform,9-puzzle,10-racing,11-real time strategy,12-role playing game,13-simulator,14-sport,15-strategy,16-turn based strategy,24-tactical,25-hack and slash/beat em up,26-quiz/trivia,30-pinball,31-adventure,32-indie,33-arcade,34-visual novel
 
-var gameGenreId = "";
-// var gameGenreId = 14;
-getPopularGames(gameGenreId);
+// & first_release_date > 1436883200
 
-function getPopularGames(genreId) {
+var gameGenreId = "";
+var gameGenreId = 14;
+var platformId = "(48)";
+// var platformId = 48;
+getPopularGames(gameGenreId, platformId);
+
+function getPopularGames(genreId, platformId) {
   criteria =
     "fields aggregated_rating,aggregated_rating_count,category,collection,first_release_date,genres,name,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,summary,total_rating,total_rating_count; limit 5; sort popularity desc; where rating > 75 & rating_count > 0 & first_release_date > 1436883200";
   if (genreId) {
-    criteria += " & genres = " + genreId + ";";
-  } else {
-    criteria += ";";
+    criteria += " & genres = " + genreId;
   }
+  if (platformId) {
+    criteria += " & platforms = " + platformId;
+  }
+  criteria += ";";
+
+  console.log(criteria);
 
   axios({
     url: "https://api-v3.igdb.com/games",
@@ -26,7 +34,7 @@ function getPopularGames(genreId) {
   })
     .then(function(response) {
       res = response.data;
-      // console.log(res);
+      console.log(res);
       for (var i = 0; i < res.length; i++) {
         console.log("Name: " + res[i].name);
         console.log("summary: " + res[i].summary);
@@ -43,7 +51,7 @@ function getPopularGames(genreId) {
 //search for users input and then output options
 var gameName = "grand theft auto";
 gameName = gameName.split(" ").join("_");
-findGameId(gameName);
+// findGameId(gameName);
 
 function findGameId(gameName) {
   var gameUrl = "https://api-v3.igdb.com/games/?search=";
@@ -71,7 +79,7 @@ function findGameId(gameName) {
 
 //have user confirm which game they want and search its id
 var gameId = 19459;
-getGameById(gameId);
+// getGameById(gameId);
 
 function getGameById(gameId) {
   var criteria =
--- public/js/questionaire.js
+++ public/js/questionaire.js
@@ -1,10 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll("select");
-  var instances = M.FormSelect.init(elems, options);
-});
-
-// Or with jQuery
-
 $(document).ready(function() {
   $("select").formSelect();
+
+  $("#submitBtn").on("click", function() {
+    console.log($("#alias").val());
+    console.log($("#videoGenre").val());
+    console.log($("#musicGenre").val());
+    console.log($("#gameGenre").val());
+    console.log(
+      $("#gameGenre")
+        .find(":selected")
+        .data("name")
+    );
+
+    console.log($("#console").val());
+    console.log(
+      $("#console")
+        .find(":selected")
+        .data("name")
+    );
+    // getPopularArtists($("#musicGenre").val());
+    // getPopularGames($("#gameGenre").val());
+    // getPopularVideos($("#videoGenre").val(), "movie");
+    // getPopularVideos($("#videoGenre").val(), "show");
+  });
 });
