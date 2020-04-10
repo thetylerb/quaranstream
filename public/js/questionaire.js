910c3cb32af9b98ac1e71f4f7db173367dc34583
keshavsavva <keshav.s.avva@gmail.com>
2020-04-10 13:40:10 -0400

    added api routes for user prefs

--- public/js/questionaire.js
+++ public/js/questionaire.js
@@ -2,31 +2,46 @@ $(document).ready(function() {
   $("select").formSelect();
 
   $("#submitBtn").on("click", function() {
-    console.log($("#alias").val());
-    console.log($("#videoGenre").val());
-    console.log($("#musicGenre").val());
-    console.log($("#gameGenre").val());
-    console.log(
-      $("#gameGenre")
-        .find(":selected")
-        .data("name")
-    );
+    event.preventDefault();
+    // console.log($("#alias").val());
+    // console.log($("#videoGenre").val());
+    // console.log($("#musicGenre").val());
+    // console.log($("#gameGenre").val());
+    // console.log(
+    //   $("#gameGenre")
+    //     .find(":selected")
+    //     .data("name")
+    // );
 
-    console.log($("#console").val());
-    console.log(
-      $("#console")
-        .find(":selected")
-        .data("name")
-    );
+    // console.log($("#console").val());
+    // console.log(
+    //   $("#console")
+    //     .find(":selected")
+    //     .data("name")
+    // );
 
     var newUser = {
-      Username: $("#alias").val(),
-      MusicGenres: $("#videoGenre").val(),
-      MovieGenres: $("#videoGenre").val(),
-      tVGenres: $("#musicGenre").val(),
-      gameGenres: $("#gameGenre").val()
+      userName: "username",
+      avatarImg: "photo",
+      enjoyMovie: true,
+      enjoyMusic: true,
+      enjoyTV: true,
+      enjoyGame: true
     };
 
+    console.log(newUser);
+    if (!newUser.userName || !newUser.avatarImg) {
+      return;
+    }
+
+    addUserPref(
+      newUser.userName,
+      newUser.avatarImg,
+      newUser.enjoyMovie,
+      newUser.enjoyMusic,
+      newUser.enjoyTV,
+      newUser.enjoyGame
+    );
     // Send the POST request.
     // $.ajax("/api/userInfo", {
     //   type: "POST",
@@ -35,4 +50,27 @@ $(document).ready(function() {
     //   location.reload();
     // });
   });
+
+  function addUserPref(
+    userName,
+    avatarImg,
+    enjoyMovie,
+    enjoyMusic,
+    enjoyTV,
+    enjoyGame
+  ) {
+    $.post("/api/userprefs", {
+      userName: userName,
+      avatarImg: avatarImg,
+      enjoyMovie: enjoyMovie,
+      enjoyMusic: enjoyMusic,
+      enjoyTV: enjoyTV,
+      enjoyGame: enjoyGame
+    }).then(function(data) {
+      // window.location.replace("/questionaire");
+      console.log(data.userName, data.enjoyMovie);
+      // If there's an error, handle it by throwing up a bootstrap alert
+    });
+    // .catch(handleLoginErr);
+  }
 });
