8f7f163743d5167cfcfbbd630d64da71a0002768
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-04-14 03:49:11 -0400

    added media api calls from node

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,9 @@
 var db = require("../models");
 var passport = require("../config/passport");
 const orm = require("../config/orm.js");
-
+var axios = require("axios");;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var gameApi = require("../public/js/game_api");
 
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
@@ -59,4 +61,146 @@ module.exports = function(app) {
       });
     }
   });
+
+
+   //game Route / Api call
+   app.post("/api/game_genres", function (req, res) {
+    console.log("in api-routes");
+    var platformId = req.body.platformId;
+    var genreId = req.body.genreId;
+    // res.json(req.body);
+    // console.log(req.body);
+    gameApi(platformId, genreId, data => {
+      console.log(data);
+      res.json(data);
+    });
+  });
+
+  function gameApi(platformId, genreId, callback) {
+    // do your api call here using axios
+    // console.log(platformId, genreId);
+    criteria =
+      "fields aggregated_rating,genres,name,platforms,rating,similar_games,summary; limit 5; sort popularity desc; where rating > 50;";
+    
+    if (genreId !== "0") {
+      gameUrl = `https://api-v3.igdb.com/games?fields=name,platforms,popularity,rating,genres&filter[genres][eq]=${genreId}&order=popularity:desc'`
+    }
+    else {
+      gameUrl = `https://api-v3.igdb.com/games?fields=name,platforms,popularity,rating,genres&order=popularity:desc'`
+    }
+  
+  
+
+    axios({
+      url: gameUrl,
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "user-key": process.env.gameApiKey,
+      },
+      // data: criteria,
+    })
+      .then(function (response) {
+        // console.log(response);
+        callback(response.data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+
+  //music Route / Api call
+  app.post("/api/music_genres", function (req, res) {
+    console.log(req.body.musicGenre);
+    var musicGenre = req.body.musicGenre;
+    musicApi(musicGenre, data => {
+      data.slice(0, 5);
+      console.log(data);
+      res.json(data);
+    });
+  });
+
+  function musicApi(genre, callback) {
+    console.log(genre);
+    var queryURL =
+      "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" +
+      process.env.musicApiKey +
+      "&format=json";
+    if (genre !== "0") {
+      queryURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists";
+      queryURL += "&tag=" + genre;
+      queryURL +=
+        "&api_key=" + process.env.musicApiKey + "&format=json";
+    }
+    console.log(queryURL);
+
+    axios({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      // console.log(response);
+      if (genre !== "0") {
+        var res = response.data.topartists.artist;
+      } else {
+        var res = response.data.artists.artist;
+      }
+      // console.log(res);
+      callback(res);
+     
+    });
+  }
+
+  //Movie Route / Api call
+  app.post("/api/movie_genres", function (req, res) {
+    var videoGenre = req.body.videoGenre;
+    videoApi(videoGenre.toLowerCase(), "movie", data => {
+      // console.log(data);
+      res.json(data);
+    });
+  });
+
+  //TV Show Route / Api call
+  app.post("/api/show_genres", function (req, res) {
+    var videoGenre = req.body.videoGenre;
+    console.log(req.body);
+    videoApi(videoGenre.toLowerCase(), "show", data => {
+      // console.log(data);
+      res.json(data);
+    });
+  });
+
+  function videoApi(genreId, type, callback) {
+    console.log(genreId);
+    var url = `https://api.trakt.tv/${type}s/trending?limit=5&extended=full`;
+    if (genreId !== "0") {
+      url += "&genres=" + genreId;
+    }
+    console.log(url);
+  
+    var request = new XMLHttpRequest();
+  
+    request.open("GET", url);
+  
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("trakt-api-version", "2");
+    request.setRequestHeader(
+      "trakt-api-key",
+      process.env.videoApiKey
+    );
+  
+    request.onreadystatechange = function() {
+      if (this.readyState === 4) {
+        // console.log("Status:", this.status);
+        // console.log("Headers:", this.getAllResponseHeaders());
+        // console.log("Body:", this.responseText);
+        var res = JSON.parse(this.responseText);
+        // console.log(res);
+        callback(res);
+    
+      }
+    };
+    request.send();
+  }
 };
+
